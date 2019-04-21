d7cf7e783a240d981dcacc0222cab53c52743666
Takanori Fukuyama <takanori.fukuyama@ngl.technology>
2019-04-21 23:50:52 +0900

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import '../App.scss';
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>緯度経度検索</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+class SearchForm extends React.Component{
+    render(){
+        return(
+            <form>
+
+            </form>
+        )
+    }
+}
+export default SearchForm;
