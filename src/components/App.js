34c381723328e58e49193b5b9e522231d37c4c65
Takanori Fukuyama <takanori.fukuyama@gmail.com>
2019-04-22 13:07:47 +0900

    3-24 Geocode API

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import axios from "axios"
 import SearchForm from "./SearchForm"
 import GeocodeResult from "./GeocodeResult"
 import '../styles/App.scss';
+
+const GEOCODE_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json?&key=AIzaSyAOaeuXU_Hlf731vA_BdOoLKwdJ-udINAI'
 
 class App extends Component {
   constructor(props){
@@ -10,9 +13,44 @@ class App extends Component {
       
     }
   }
+  setErrorMessage = message => {
+    this.setState({
+      address: message,
+      lat: 0,
+      lng: 0,
+    })
+  }
   
   handlePlaceSubmit = place => {
-    console.log(place);
+    axios.get(GEOCODE_ENDPOINT, { params: { address: place }})
+    .then( results => {
+      //success
+      console.log(results);
+      const data = results.data
+      const result = data.results[0]
+      switch(data.status){
+        case 'OK': {
+          const location = result.geometry.location
+          this.setState({
+            address: result.formatted_address,
+            lat: location.lat,
+            lng: location.lng,
+          })
+          break;
+        }
+        case 'ZERO_RESULTS':{
+          this.setErrorMessage("Not founded.")
+          break;
+        }
+        default: {
+          this.setErrorMessage("Error!!")
+          break;
+        }
+      }
+    })
+    .catch(() => {
+      this.setErrorMessage("通信エラー")
+    })
     
   }
   render() {
